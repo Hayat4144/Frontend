8c3f4aa681976ff8486ccc97d1c34fdd2b7abd39
hayat4144 <ihayat855@gmail.com>
2022-12-25 18:59:04 +0530

    Slider product is added.

--- src/shop/SlidersProducts.jsx
+++ src/shop/SlidersProducts.jsx
@@ -7,14 +7,14 @@ export default function SlidersProducts() {
             id: 1,
             name: 'Product 1',
             price: 34,
-            image: ProductImage
+            image: "https://m.media-amazon.com/images/I/51iiN42STEL._SX1500_.jpg"
 
         },
         {
             id: 2,
             name: 'Product 2',
             price: 60,
-            image: ProductImage
+            image: "https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg"
 
         },
         {
@@ -26,20 +26,28 @@ export default function SlidersProducts() {
         }
     ]
     return (
-        <div className='home_products flex flex-col items-center
-        w-full py-10 justify-end mx-5'>
-            <div className='product-info'>
-                <h2 className='product-name'>
-                    Product 1
-                </h2>
-                <h3 className='price'>Rs 455</h3>
-            </div>
-            <figure className='product-image'>
-                <img
-                    className='w-full max-h-[280px]
-                    object-contain mb-5 mt-5'
-                    src={ProductImage} />
-            </figure>
+        <div className='home_products grid grid-cols-1 sm:grid-cols-2
+        w-full md:grid-cols-3 lg:grid-cols-4 py-10 gap-5'>
+            {
+                [1, 2, 3, 4].map((item) => (
+                    <div className='items bg-white mb-5 px-4 py-5'>
+                        <div className='product-info cursor-pointer'>
+                            <h3 className='card_title text-[17px] font-bold leading-tight'>
+                                Shop by Category
+                            </h3>
+                            <figure className='images overflow-hidden py-5'>
+                                <img src={ProductImage} alt="image"
+                                    className='w-full h-52' />
+                            </figure>
+                            <a className='see_more text-blue-700 cursor-pointer
+                            hover:underline text-[18px]'
+                            >see more</a>
+                        </div>
+                    </div>
+
+                ))
+            }
+
         </div>
     )
 }
