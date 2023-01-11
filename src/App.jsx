80185f8bcc2c8f56b75c9745a447f06d3673d33b
hayat4144 <ihayat855@gmail.com>
2023-01-11 14:10:03 +0530

    password reset route is added.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import ProductPage from "./shop/ProductPage";
 import ProductSearch from "./shop/ProductSearch";
 import Products from "./shop/AllProducts";
-import Navbar from "./UsableComponent/Navbar";
 import Cart from "./shop/Cart/Cart";
 import AccountContainer from "./Accounts/AccountContainer";
 import Checkout from "./shop/Checkout/Checkout";
@@ -19,12 +18,14 @@ import ConfrimOrder from "./shop/Checkout/ConfrimOrder";
 import { lazy, Suspense } from "react";
 import ProductByCategory from "./shop/Category/ProductByCategory";
 const Payment = lazy(() => import('./shop/Checkout/Payment'))
+const UserOrdersHistory = lazy(() => import('./Accounts/Order'))
+const ForgetPassword = lazy(() => import('./Accounts/ForgetPassword'))
+const ForgetPasswordRequest = lazy(() => import('./Accounts/RequestForgetpassword'))
 
 function App() {
 
   return (
     <div className="App">
-
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -32,14 +33,17 @@ function App() {
           <Route path="/V2/shop/checkout" element={<Checkout />} />
           <Route path="V2/shop/checkout/confirm/order" element={<ConfrimOrder />} />
           <Route path="/V2/shop/products/category" element={<ProductByCategory />} />
-          <Route path="V2/shop/checkout/payment" element={<Suspense fallback={<p>laoding</p>}><Payment /></Suspense>} />
+          <Route path="/V2/shop/checkout/payment" element={<Suspense fallback={<p>laoding</p>}><Payment /></Suspense>} />
+          <Route path="/V2/user/account/order/history" element={<Suspense fallback={<p>laoding</p>}><UserOrdersHistory /></Suspense>} />
+          <Route path="/v2/auth/user/change/password/link/verify/:user/:token" element={<Suspense fallback={<p>laoding</p>}><ForgetPassword /></Suspense>} />
+          <Route path="/V2/forget/password/request" element={<Suspense fallback={<p>laoding</p>}><ForgetPasswordRequest /></Suspense>} />
           <Route path="/V2/user/account" element={<AccountContainer />} />
           <Route path="/V2/account/profile" element={<Profile />} />
           <Route path="/V2/account/address" element={<Address />} />
           <Route path="/V2/account/change/password" element={<ChangePassword />} />
           <Route path="/V2/auth/sign_up" element={<Signup />} />
           <Route path="/V2/auth/sign_in" element={<Signin />} />
-          <Route path="/V2/shop/product/:id/:name" element={<ProductPage />} />
+          <Route path="/V2/shop/product/:id/:name/:category" element={<ProductPage />} />
           <Route path="/V2/shop/search" element={<ProductSearch />} />
           <Route path="/V2/shop/products" element={<Products />} />
           <Route path='*' element={<NotFound />} />
