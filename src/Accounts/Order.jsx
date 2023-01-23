338fcac12d76040801584304f2b70344ccd22cb4
Hayat ilyas <ihayat855@gmail.com>
2023-01-23 17:15:39 +0530

    order page fetching and showing functionality.

--- src/Accounts/Order.jsx
+++ src/Accounts/Order.jsx
@@ -5,11 +5,29 @@ import { lazy } from 'react'
 const Navbar = lazy(() => import('../UsableComponent/Navbar'))
 const Footer = lazy(() => import('../UsableComponent/Footer'))
 import NavbarSkeleton from '../Skeleton/NavbarSkeleton'
-
-
+import { useNavigate,createSearchParams ,useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Order() {
-    const [Orderdata, setOrderdata] = useState(null)
+    //  -------------------- All states --------------------- //
+    const [Orderdata, setOrderdata] = useState([])
+    const [produceVarient, setProduceVarient] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const { IsLogdin } = useSelector(state => state.Signin)
+    const Location = useLocation();
+    const navigate = useNavigate();
+
+    // ------------------------ check is user Logdin or not -------------- //
+   
+        useEffect(() => {
+            !IsLogdin ? navigate({
+                pathname: "/V2/auth/sign_in",
+                search: `?${createSearchParams({ 'next': Location.pathname })}`
+            }) : null;
+        }, [])
+  
+
+    //  ---------------------- fetch order of user --------------------- //
     const FetchOrderHistory = async () => {
         await fetch('http://localhost:5000/v3/api/user/orders/history', {
             method: 'GET',
@@ -19,29 +37,50 @@ export default function Order() {
             credentials: 'include'
         })
             .then(async res => {
-                const { error, data } = await res.json()
-                if (res.status === 200) {
-                  const result =  data.map(item => {
-                        const date = new Date(item.created_at);
-                        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-                        const humanReadableDate = date.toLocaleDateString('en-US', options);
-                        return {...item , created_at:humanReadableDate}
+                const { error, data } = await res.json() ;
+                if(res.status !== 200){
+                    setIsLoading(false);
+                    return ;
+                } 
+                //  ------------------- make the human readable date formate and push in the existing data --------- //
+                const result = data.map(item => {
+                    const date = new Date(item.created_at);
+                    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+                    const humanReadableDate = date.toLocaleDateString('en-US', options);
+                    return { ...item, created_at: humanReadableDate }
 
-                    })
-                    setOrderdata(result)
-                }
-                else {
-                    setOrderdata(null)
-                }
+                })
+
+                //  -------------------- iterating over data to get varient and push into an array for fetching products ----------- //
+                let varients = []
+                result.forEach(element => {
+                    element.products.forEach(vairentitem => {
+                      varients.push(vairentitem.varientId)
+                    });
+                });
+                setOrderdata(result)
+
+                //  ----------- fetch products data --------------------- //
+                await fetch('http://localhost:5000/v4/api/get/products/varients/order/history', {
+                    method:"POST",
+                    headers:{
+                        'Content-Type':"application/json"
+                    },
+                    body:JSON.stringify({varients}),
+                    credentials:"include"
+                }).then(async response=>{
+                    const result = await response.json();
+                    if(response.status !== 200) return setIsLoading(false)
+                    setProduceVarient(result.data)
+                    setIsLoading(false)
+                    
+                }).catch(error=> console.log(error))
 
             }).catch(err => console.log(err))
     }
     useEffect(() => {
-
         FetchOrderHistory()
     }, [])
-   
-    // console.log(Orderdata);
 
     return (
         <Fragment>
@@ -50,24 +89,25 @@ export default function Order() {
             </Suspense>
             <section className="order_history_container my-10 mx-10">
                 {
-                    Orderdata.map(item => (
-                        <div key={item._id} className="order_history_box border my-10 border-gray-300 shadow-sm rounded-md">
-                            <div className='order_history_box_header border-b border-gray-300 flex justify-between items-center px-5 h-24 py-10 '>
-                                <div className='order_id'>
-                                    <h3 className='orderId_text font-extrabold'>Order Id</h3>
-                                    <span className='order_number text-gray-600'>{item._id}</span>
-                                </div>
-                                <div className='order_totalPrice'>
-                                    <h3 className='ordertotalPrice_text font-extrabold'>Total Amount</h3>
-                                    <span className='order_totalpriceValue text-gray-600'>{item.totalPrice}</span>
-                                </div>
-                                <div className='order_date'>
-                                    <h3 className='orderDate_text font-extrabold'>Order Date</h3>
-                                    <span className='order_OrderDate text-gray-600'>{item.created_at }</span>
+                  isLoading ?  <p>loading....</p>:  Orderdata.length > 0 ?
+                        Orderdata.map(item => (
+                            <div key={item._id} className="order_history_box border my-10 border-gray-300 shadow-sm rounded-md">
+                                <div className='order_history_box_header border-b border-gray-300 flex justify-between items-center px-5 h-24 py-10 '>
+                                    <div className='order_id'>
+                                        <h3 className='orderId_text font-extrabold'>Order Id</h3>
+                                        <span className='order_number text-gray-600'>{item._id}</span>
+                                    </div>
+                                    <div className='order_totalPrice'>
+                                        <h3 className='ordertotalPrice_text font-extrabold'>Total Amount</h3>
+                                        <span className='order_totalpriceValue text-gray-600'>{item.totalPrice}</span>
+                                    </div>
+                                    <div className='order_date'>
+                                        <h3 className='orderDate_text font-extrabold'>Order Date</h3>
+                                        <span className='order_OrderDate text-gray-600'>{item.created_at}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
+                        )) : <p>no order history</p> 
                 }
 
             </section>
