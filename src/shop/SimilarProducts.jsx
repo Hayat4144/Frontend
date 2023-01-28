7fa934c124e48849d772e1ae7c72a882fde2316c
Hayat ilyas <ihayat855@gmail.com>
2023-01-28 12:09:03 +0530

    apply loading skeleton.

--- src/shop/SimilarProducts.jsx
+++ src/shop/SimilarProducts.jsx
@@ -2,23 +2,32 @@ import React, { Fragment, useState } from 'react'
 import { useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import { Rating } from '@mui/material'
+import SampleProductSkeleton from '../Skeleton/SampleProductSkeleton';
 
 export default function SimilarProducts() {
+    const [isLoading, setIsLoading] = useState(false)
     const [product_detail, setProduct_detail] = useState([])
+    const [noProduct, setNoProduct] = useState(false)
     const { id, name, category } = useParams();
 
     const navigate = useNavigate();
 
     // --------- fetching products ----------------- //
     const fetchData = () => {
+        setIsLoading(!isLoading)
         fetch(`${import.meta.env.VITE_BACKEND_URL}/v4/api/get/similar/product?productId=${id}&categoryId=${category}&search=${name}`, {
             method: "GET",
             credentials: 'include',
         }).then(async (res) => {
-            if (res.status === 200) {
-                const { data } = await res.json();
-                setProduct_detail(data)
+            const { data } = await res.json();
+            if (data.length < 1) {
+                setNoProduct(true);
+                setIsLoading(false);
+                return;
             }
+            setIsLoading(!isLoading);
+            setProduct_detail(data)
         })
             .catch(error => console.log(error))
     }
@@ -37,30 +46,42 @@ export default function SimilarProducts() {
                         <AiOutlineArrowRight />
                     </h3>
                 </div>
-                <div className="product_details_contianer grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                    {
-                        product_detail.map((item, index) => (
-                            <div className="product_detaiils" key={item._id}>
-                                <figure className='overflow-hidden rounded-md'>
-                                    <img src={item.assets.images[0]} alt="product-pic"
-                                        className='w-full h-56 rounded-md hover:scale-125  transition ease-in-out duration-500' />
-                                </figure>
-                                <div className='product_details my-2' onClick={() => {
-                                    navigate(`/V2/shop/product/${item._id}/${item.name}/${item.category}`)
-                                }}>
-                                    <p className='product_name capitalize w-full 
-                                    text-[17px] hover:text-indigo-700'>
-                                        {item.name}
-                                    </p>
-                                    <p className='product_price text-[18px]'>
-                                        <span className='currency_symbol'> Rs </span>
-                                        {item.price}
-                                    </p>
-                                </div>
+                {isLoading ?
+                    <SampleProductSkeleton />
+                    : noProduct ?
+                        <div className='my-10 h-20 '>
+                            <h2 className='text-xl text-center mb-5'>
+                                No product found
+                            </h2>
+                        </div>
+                        : <Fragment>
+                            <div className='sample_product_container mx-2  md:px-5 px-2 py-5 mb-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                                {
+                                    product_detail.map((item, index) => (
+                                        <div className="product_detaiils" key={item._id}>
+                                            <figure className='overflow-hidden'>
+                                                <img src={item.assets.images[0]} alt="product-pic"
+                                                    className='w-full h-56  hover:scale-125  transition ease-in-out duration-500' />
+                                            </figure>
+                                            <div className='product_details my-2' onClick={() => {
+                                                navigate(`/V2/shop/product/${item._id}/${item.name}/${item.category}`)
+                                            }}>
+                                                <p className='product_name capitalize w-full justify-between cursor-pointer
+                                    text-xl hover:text-indigo-700 flex items-center  font-extrabold'>
+                                                    <span>{item.name}</span>
+                                                    <span className='currency_symbol'> Rs {item.price} </span>
+                                                </p>
+                                                <h1 className='product_rating my-2  text-[18px]'>
+                                                    <Rating value={item.average_rating} />
+                                                </h1>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
                             </div>
-                        ))
-                    }
-                </div>
+
+                        </Fragment>
+                }
             </div>
         </Fragment>
     )
