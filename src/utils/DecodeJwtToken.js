996bddb1153b7d5ec9126baff971419b3cd027a0
Hayat ilyas <ihayat855@gmail.com>
2023-01-29 23:21:30 +0530

    remove js-cookie

--- src/utils/DecodeJwtToken.js
+++ src/utils/DecodeJwtToken.js
@@ -1,14 +1,14 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 import jwt_decode from 'jwt-decode'
 import { SIGNIN, USERINFO } from '../Context/Actions/ActionType';
 const DecodeJwtToken = (dispatch) => {
     console.log(document.cookie)
-    const jwt_token = Cookies.get('token');
-    if (jwt_token) {
-        const decoded_token = jwt_decode(jwt_token);
-        dispatch({ type: SIGNIN })
-        dispatch({ type: USERINFO, payload: decoded_token })
-    }
+    // const jwt_token = Cookies.get('token');
+    // if (jwt_token) {
+    //     const decoded_token = jwt_decode(jwt_token);
+    //     dispatch({ type: SIGNIN })
+    //     dispatch({ type: USERINFO, payload: decoded_token })
+    // }
 
 
 }
