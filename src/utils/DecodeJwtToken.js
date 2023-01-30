b29bdc26985815c06b7cd2931f993a16a836c866
Hayat ilyas <ihayat855@gmail.com>
2023-01-30 13:03:28 +0530

    rename the token

--- src/utils/DecodeJwtToken.js
+++ src/utils/DecodeJwtToken.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie'
 import jwt_decode from 'jwt-decode'
 import { SIGNIN, USERINFO } from '../Context/Actions/ActionType';
 const DecodeJwtToken = (dispatch) => {
-    const jwt_token = Cookies.get(import.meta.env.DEV ? 'token_dev' : 'token_production');
+    const jwt_token = Cookies.get(import.meta.env.DEV ? 'token_dev' : 'token');
     if (jwt_token) {
         const decoded_token = jwt_decode(jwt_token);
         dispatch({ type: SIGNIN })
