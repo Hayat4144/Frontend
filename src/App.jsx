19022bc3eea9d907c47c03cc07da0993b82b55ac
Hayat ilyas <ihayat855@gmail.com>
2023-03-15 12:49:14 +0530

    remove clear function.

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ import WholeProductpage from "./Skeleton/WholeProductpage";
 import { LOGOUT } from "./Context/Actions/ActionType";
 
 function App() {
-  console.clear();
+  // console.clear();
   return (
     <div className="App">
       <Router>
